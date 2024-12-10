871aee991e85383caec7c1124dfbdd4c22829234
Lin <100497279+Collinb190@users.noreply.github.com>
2024-12-10 02:34:00 -0600

    interactive webpage

---
+++ particles.js
@@ -0,0 +1,29 @@
+window.onload = function() {
+    particlesJS('particles-js', {
+      "particles": {
+        "number": {
+          "value": 80
+        },
+        "size": {
+          "value": 3
+        },
+        "move": {
+          "speed": 2
+        },
+        "line_linked": {
+          "enable": true
+        },
+        "color": {
+          "value": "#ffffff"
+        }
+      },
+      "interactivity": {
+        "events": {
+          "onhover": {
+            "enable": true,
+            "mode": "repulse"
+          }
+        }
+      }
+    });
+  };
